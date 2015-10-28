3253303a0e8b513851bb183a5090d2bb1e1a92f8
Rami <ammz93@gmail.com>
2015-10-28 09:51:24 +0100

    Hyukchan, my first commit!!

--- src/com/KoMark/Koala/MainActivity.java
+++ src/com/KoMark/Koala/MainActivity.java
@@ -8,19 +8,28 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends Activity implements SensorEventListener {
 
     private SensorManager mSensorManager;
     private Sensor mSensor;
-
+    private ListView lv;
+    private ArrayList<String> readings = new ArrayList<String>();
+    private ArrayAdapter<String> arrayAdapter;
     long lastUpdate;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-
+        lv = (ListView) findViewById(R.id.listView);
+        readings.add("Measurements:");
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, readings);
+        lv.setAdapter(arrayAdapter);
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
     }
@@ -52,6 +61,9 @@ public class MainActivity extends Activity implements SensorEventListener {
                 lastUpdate = curTime;
 
                 Log.e("Acc", "x:" + x + ", y: " + y + ", z: " + z);
+                readings.add("x: "+x+", y: "+y+", z: "+z);
+                arrayAdapter.notifyDataSetChanged();
+
             }
         }
     }
