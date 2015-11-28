fec0136b8e0d2d2e14488c5c27465e6e40f0cdf0
Rami <ammz93@gmail.com>
2015-11-28 05:56:47 +0100

    SerialUID for KGROUPDATA

--- src/com/KoMark/Koala/data/KGroupData.java
+++ src/com/KoMark/Koala/data/KGroupData.java
@@ -2,14 +2,16 @@ package com.KoMark.Koala.data;
 
 import android.bluetooth.BluetoothDevice;
 
+import java.io.Serializable;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 /**
  * Created by TeraByte on 08-11-2015.
  */
-public class KGroupData {
+public class KGroupData implements Serializable {
 
+    private static final long serialVersionUID = -3088240274207500418L;
     ArrayList<String> deviceNames = new ArrayList<>();
     ArrayList<String> deviceAddresses = new ArrayList<>();
 
