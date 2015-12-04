4983f213cd14e76be669fd7112c897a14e991e18
Hyukchan Kwon <hyukchan.k@gmail.com>
2015-12-04 15:15:49 +0900

    Test bind service

--- src/com/KoMark/Koala/KoalaApplication.java
+++ src/com/KoMark/Koala/KoalaApplication.java
@@ -1,31 +1,50 @@
 package com.KoMark.Koala;
 
 import android.app.Application;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.util.Log;
 
 import com.KoMark.Koala.core.KoalaManager;
 import com.KoMark.Koala.core.KoalaService;
+import com.KoMark.Koala.ui.MainActivity;
 
 /**
  * Created by Hyukchan on 03/11/2015.
  */
 public class KoalaApplication extends Application {
 
+    private final String CLASS_TAG = "KApplication";
     private KoalaManager koalaManager;
     private static Context context;
+    private KoalaService kService = null;
+    private ServiceConnection servConn = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            Log.i(CLASS_TAG, "Service connected: "+name);
+            kService = ((KoalaService.KServiceBinder)service).getService();
+            kService.initKManager(context);
+            activityContext.setListeners();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            Log.i(CLASS_TAG, "Service disconnected: "+name);
+        }
+    };
+    private MainActivity activityContext;
 
     @Override
     public void onCreate() {
         super.onCreate();
         context = this;
+        startService((new Intent(this, KoalaService.class)).setFlags(Intent.FLAG_RECEIVER_FOREGROUND));
+        bindService(new Intent(this, KoalaService.class).setFlags(Intent.FLAG_RECEIVER_FOREGROUND), servConn, BIND_AUTO_CREATE);
 
-        if(koalaManager == null) {
-            koalaManager = new KoalaManager();
-            koalaManager.initializeComponents(this);
-            startService(new Intent(this, KoalaService.class));
-        }
+
     }
 
     @Override
@@ -35,9 +54,18 @@ public class KoalaApplication extends Application {
         //koalaManager.shutdown();
     }
 
+
+
     public KoalaManager getKoalaManager() {
         return koalaManager;
     }
 
 
+    public void setKManager(KoalaManager KManager) {
+        this.koalaManager = KManager;
+    }
+
+    public void setActivityContext(MainActivity activityContext) {
+        this.activityContext = activityContext;
+    }
 }
--- src/com/KoMark/Koala/core/KoalaService.java
+++ src/com/KoMark/Koala/core/KoalaService.java
@@ -3,7 +3,10 @@ package com.KoMark.Koala.core;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 
@@ -15,17 +18,39 @@ import com.KoMark.Koala.R;
  */
 public class KoalaService extends Service {
     KoalaManager koalaManager;
+    private final String CLASS_TAG = "KService";
+    private final IBinder myBinder = new KServiceBinder();
+
+
 
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        Log.i(CLASS_TAG, "OnBind called");
+        return myBinder;
+    }
+
+    public void initKManager(Context context) {
+        if(koalaManager == null) {
+            koalaManager = new KoalaManager();
+            ((KoalaApplication) context).setKManager(koalaManager);
+            koalaManager.initializeComponents(context);
+            Log.i(CLASS_TAG, "Created new KManager");
+        } else {
+            ((KoalaApplication) context).setKManager(koalaManager);
+            Log.i(CLASS_TAG, "Bound to old KManager");
+        }
+    }
+
+    public class KServiceBinder extends Binder {
+        public KoalaService getService() {
+            return KoalaService.this;
+        }
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
 
-        koalaManager = ((KoalaApplication) getApplicationContext()).getKoalaManager();
         Intent notificationIntent = new Intent(this, KoalaService.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
         Notification.Builder build = new Notification.Builder(getApplicationContext());
@@ -36,21 +61,22 @@ public class KoalaService extends Service {
                 .setWhen(System.currentTimeMillis());
         Notification n = build.getNotification();
         startForeground(999, n);
-        Log.i("KoalaService", "OnCreate executed");
+        Log.i(CLASS_TAG, "OnCreate executed");
     }
 
 
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        return super.onStartCommand(intent, flags, startId);
+        return START_STICKY;
+        //return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        unregisterReceiver(koalaManager.kComm);
         Log.i("KService", "OnDestroy called.");
+        unregisterReceiver(koalaManager.kComm);
         koalaManager.shutdown();
     }
 
